e68c5cc881c51daf4fbafcb0a741e0bcee10ab48
Amine A. Andaloussi <a.abbadandaloussi@gmail.com>
2022-10-16 16:10:25 +0200

    README updated

--- ExternalUtils/SessionsGenerator/test.py
+++ ExternalUtils/SessionsGenerator/test.py
@@ -6,16 +6,16 @@ import re
 
 experimentConfig = {
 
-	"rooms": ["A","M"],
-	"linkingSubProcessesModes": ["no","newTab","withinTab"],
-	"participantsPerRoom": 100,
-	"ParticipantsIdPrefix": "P",
-	"participantsIdSeed":1,
+	"rooms": ["A","M"], #eye-tracking rooms where parrallel sessions will occur (e.g., Amine room A, Maike room M)
+	"participantsPerRoom": 50, #number of participant sessions to be created in each room folder
+	"ParticipantsIdPrefix": "P", #prefix used in participant id
+	"participantsIdSeed":1, #seed from which to start when assigning ids to participants
 	"shuffleStartIndex":2, #shuffle questions list starting from startIndex=2 (to keep the amazon smart process questions first)
 
-	"template" :  'templates/pilot-session.json',
-	"outDirectory": "out/"
 
+	"template" :  '../../Experiment-data/template.json', #template file path
+	"outDirectory": "../../Experiment-data/Participant-data/", #output directory
+	"linkingSubProcessesModes": ["no","newTab","withinTab"] #linking modes accronyms for no-support, symoblic links, breadcrumb respectively
 }
 
 

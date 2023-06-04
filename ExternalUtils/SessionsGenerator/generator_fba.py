d2a83f4ac102efa29439dd3a70390ab631c8e2e2
Amine Abbad-Andaloussi <amine.abbad-andaloussi@unisg.ch>
2023-06-04 19:50:32 +0200

    different session generators for different experiments

--- ExternalUtils/SessionsGenerator/generator_fba.py
+++ ExternalUtils/SessionsGenerator/generator_fba.py
@@ -2,16 +2,16 @@ from utils import  isDirectoryEmpty, createSessions
 
 experimentConfig = {
 
-	"rooms": ["A","M"], #eye-tracking rooms where parrallel sessions will occur
+	"rooms": ["S","K"], #eye-tracking rooms where parrallel sessions will occur
 	"participantsPerRoom": 50, #number of participant sessions to be created in each room folder
 	"ParticipantsIdPrefix": "P", #prefix used in participant id
 	"participantsIdSeed":1, #seed from which to start when assigning ids to participants
 	"shuffleStartTaskIndex":2, #shuffle tasks list starting from startIndex=2 
-	"questionsPerTask": 1, #number of questions per task. Note that the questions within the tasks are not randomized. Only the tasks are randomized
+	"questionsPerTask": 3, #number of questions per task. Note that the questions within the tasks are not randomized. Only the tasks are randomized
 
-	"template" :  '../../Experiment-data/template.json', #template file path
-	"outDirectory": "../../Experiment-data/Participant-data/", #output directory
-	"linkingSubProcessesModes": ["no","newTab","withinTab"] # ["no","newTab","withinTab"], linking modes accronyms for no-support, symoblic links, breadcrumb respectively
+	"template" :  '../../Experiments-data/bpmn-fba-2023/template.json', #template file path
+	"outDirectory": "../../Experiments-data/bpmn-fba-2023/Participant-data/", #output directory
+	"linkingSubProcessesModes": ["no"] # ["no","newTab","withinTab"], linking modes accronyms for no-support, symoblic links, breadcrumb respectively
 }
 
 # check that experimentConfig["outDirectory"] is empty

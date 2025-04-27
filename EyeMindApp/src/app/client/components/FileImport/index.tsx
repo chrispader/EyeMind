9439d57a21c7c8ca150cd97e2b3a74c18cbe37b6
Christoph Pader <chris@margelo.com>
2025-04-28 00:49:38 +0200

    move and simplify FileImport component

--- EyeMindApp/src/app/client/components/FileImport/index.tsx
+++ EyeMindApp/src/app/client/components/FileImport/index.tsx
@@ -1,10 +1,16 @@
-import { useCallback, useEffect, useRef } from 'react'
+import { useCallback, useState } from 'react'
+import { traverseItem } from '@/app/client/components/FileImport/traverseItem'
 import { containerClasses } from '@/app/client/css/styles'
 import { nFiles, shiftFile } from '@/app/client/modules/dataModels/filesBuffer'
 import { setFiles } from '@/app/client/modules/dataModels/filesBuffer'
 import { useStateStore } from '@/app/client/modules/dataModels/state'
-import { traverseItem } from '@/app/client/modules/ui/files-setup'
 import { cancelDefault, errorAlert } from '@/app/client/modules/utils/utils'
+
+declare global {
+  interface DataTransfer {
+    isForTestingPurpose: boolean
+  }
+}
 
 type FileImportProps = {
   uploadLabel: string
@@ -17,9 +23,16 @@ function FileImport({
 }: FileImportProps): React.ReactElement {
   const { state } = useStateStore((state) => state)
 
-  const uploadZoneRef = useRef<HTMLDivElement>(null)
+  const [isActive, setIsActive] = useState(false)
+  const [filesInternal, setFilesInternal] = useState<FileList | undefined>(undefined)
+
+  console.log({ filesInternal })
 
   const handleDroppedFiles = useCallback(async (e: React.DragEvent<HTMLDivElement>) => {
+    console.log('onDrop')
+
+    setIsActive(false)
+
     // a hack to support the testing of a single file upload using the drag/drop feature as the testing library playwright have an issue with webkitGetAsEntry returning always null
     if (e.dataTransfer.isForTestingPurpose) {
       traverseItem(e.dataTransfer.files[0])
@@ -31,18 +44,16 @@ function FileImport({
     // get dropped files
     const files = e.dataTransfer.files
 
+    setFilesInternal(files)
     setFiles(files)
 
-    //  differ execution denpending on the state.importMode
     if (state.importMode == 'multiple') {
       console.log('multiple files import mode')
       console.log('files', files)
 
       // traverse first file item
       await traverseItem(shiftFile())
-    }
-    // state.importMode=="single"
-    else if (state.importMode == 'single') {
+    } else if (state.importMode == 'single') {
       console.log('single file import mode')
 
       // ensure that you have only one single item
@@ -59,35 +70,21 @@ function FileImport({
     }
   }, [])
 
-  useEffect(() => {
-    uploadZoneRef
-    /// drag and drop event listeners
-    uploadZoneRef.current?.addEventListener('dragover', (e) => {
-      e.preventDefault()
-      e.stopPropagation()
-
-      e.dataTransfer.dropEffect = 'copy'
-    })
-
-    uploadZoneRef.current?.addEventListener('drop', handleDroppedFiles)
-  })
-
   return (
     <div className={`${containerClasses} import-view`} id="import-view">
       <div className="import-box" id="import-box">
         <div
-          className="upload-zone"
+          className={`upload-zone ${isActive ? 'upload-zone-active' : ''}`}
           id="upload-zone"
-          ref={uploadZoneRef}
-          onDragOver={() =>
-            document.getElementById('upload-zone')?.setAttribute('drop-active', 'true')
-          }
-          onDragLeave={() =>
-            document.getElementById('upload-zone')?.setAttribute('drop-active', 'false')
-          }
-          onDrop={() =>
-            document.getElementById('upload-zone')?.setAttribute('drop-active', 'false')
-          }>
+          onDrop={handleDroppedFiles}
+          onDragEnter={() => setIsActive(true)}
+          onDragOver={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+
+            e.dataTransfer.dropEffect = 'copy'
+          }}
+          onDragLeave={() => setIsActive(false)}>
           <span id="upload-label" className="upload-label">
             {uploadLabel}
           </span>
@@ -97,6 +94,7 @@ function FileImport({
           <button
             className="process-files-btn"
             id="process-files"
+            disabled={(filesInternal?.length ?? 0) == 0}
             onClick={onProcessFiles}>
             Load files
           </button>

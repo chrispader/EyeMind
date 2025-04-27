09377a24e2e0a9de270655b94e2052d721537818
Christoph Pader <chris@margelo.com>
2025-04-28 00:50:14 +0200

    add type for ModelFile

---
+++ EyeMindApp/src/app/client/model/Files.ts
@@ -0,0 +1,10 @@
+export type ModelFile = {
+  id?: string
+  fileName?: string
+  path?: string
+  xml?: string
+  isMain?: boolean
+  groupId?: string
+  unclosable?: boolean
+  mainTab?: boolean
+}

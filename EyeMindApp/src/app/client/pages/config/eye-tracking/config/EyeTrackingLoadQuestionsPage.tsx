84020de8f2022e24516e187e546d99239d43c8a8
Christoph Pader <chris@margelo.com>
2025-04-28 22:23:39 +0200

    update FileImport pages

--- EyeMindApp/src/app/client/pages/config/eye-tracking/config/EyeTrackingLoadQuestionsPage.tsx
+++ EyeMindApp/src/app/client/pages/config/eye-tracking/config/EyeTrackingLoadQuestionsPage.tsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router'
 import { ROUTES } from '@/app/client/ROUTES'
 import FileImport from '@/app/client/components/FileImport'
+import { loadFiles } from '@/app/client/components/FileImport/loadFile'
+import { errorAlert } from '@/app/client/modules/utils/utils'
 import { useStateStore } from '@/app/client/state/state'
 
 export function EyeTrackingLoadQuestionsPage(): React.ReactElement {
@@ -23,7 +25,17 @@ export function EyeTrackingLoadQuestionsPage(): React.ReactElement {
       expectedArtifact="questions"
       expectedExtensions={['csv']}
       uploadLabel="Drop a questions csv file"
-      onFilesLoaded={() => {
+      onDrop={(files, config) => {
+        if (files.length > 1) {
+          const msg = 'only a single file can be imported' // check third argument
+          console.error(msg)
+          errorAlert(msg)
+          return
+        }
+
+        loadFiles(files, config)
+      }}
+      onLoad={() => {
         navigate(ROUTES.EYE_TRACKING_EXPERIMENT)
       }}
     />
--- EyeMindApp/src/app/client/pages/config/eye-tracking/config/EyeTrackingLoadSessionPage.tsx
+++ EyeMindApp/src/app/client/pages/config/eye-tracking/config/EyeTrackingLoadSessionPage.tsx
@@ -1,9 +1,14 @@
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { ROUTES } from '@/app/client/ROUTES'
 import FileImport from '@/app/client/components/FileImport'
+import { loadFiles } from '@/app/client/components/FileImport/loadFile'
+import { errorAlert } from '@/app/client/modules/utils/utils'
 import { useStateStore } from '@/app/client/state/state'
 
 export function EyeTrackingLoadSessionPage(): React.ReactElement {
   const { setState } = useStateStore.getState()
+  const navigate = useNavigate()
 
   // TODO: Remove once state is split up
   useEffect(() => {
@@ -21,6 +26,19 @@ export function EyeTrackingLoadSessionPage(): React.ReactElement {
       expectedArtifact="session"
       expectedExtensions={['json']}
       uploadLabel="Drop a session file"
+      onDrop={(files, config) => {
+        if (files.length > 1) {
+          const msg = 'only a single file can be imported' // check third argument
+          console.error(msg)
+          errorAlert(msg)
+          return
+        }
+
+        loadFiles(files, config)
+      }}
+      onLoad={() => {
+        navigate(ROUTES.EYE_TRACKING_EXPERIMENT)
+      }}
     />
   )
 }

ebd2f95678fd984d8b31f130f27d3319f9eef1db
Christoph Pader <chris@margelo.com>
2025-04-28 15:42:57 +0200

    move config pages

--- EyeMindApp/src/app/client/pages/config/eye-tracking/config/EyeTrackingLoadQuestionsPage.tsx
+++ EyeMindApp/src/app/client/pages/config/eye-tracking/config/EyeTrackingLoadQuestionsPage.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { ROUTES } from '@/app/client/ROUTES'
 import FileImport from '@/app/client/components/FileImport'
 import { useStateStore } from '@/app/client/state/state'
 
 export function EyeTrackingLoadQuestionsPage(): React.ReactElement {
   const { setState } = useStateStore.getState()
-
+  const navigate = useNavigate()
   // TODO: Remove once state is split up
   useEffect(() => {
     setState({
@@ -21,6 +23,9 @@ export function EyeTrackingLoadQuestionsPage(): React.ReactElement {
       expectedArtifact="questions"
       expectedExtensions={['csv']}
       uploadLabel="Drop a questions csv file"
+      onFilesLoaded={() => {
+        navigate(ROUTES.EYE_TRACKING_EXPERIMENT)
+      }}
     />
   )
 }

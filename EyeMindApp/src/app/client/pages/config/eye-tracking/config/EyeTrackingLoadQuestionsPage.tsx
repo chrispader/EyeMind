4b177f6092bbc8c1464bba1761ba75641db96ef8
Christoph Pader <christoph.pader@outlook.com>
2025-12-26 14:24:48 +0000

    fix: question loading React migartion

--- EyeMindApp/src/app/client/pages/config/eye-tracking/config/EyeTrackingLoadQuestionsPage.tsx
+++ EyeMindApp/src/app/client/pages/config/eye-tracking/config/EyeTrackingLoadQuestionsPage.tsx
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router'
 import { ROUTES } from '@/app/client/ROUTES'
 import FileImport from '@/app/client/components/FileImport'
 import { loadFiles } from '@/app/client/components/FileImport/loadFile'
+import { LoadFileConfig } from '@/app/client/components/FileImport/types'
 import { errorAlert } from '@/app/client/modules/utils/utils'
 import { useStateStore } from '@/app/client/state/state'
 
 export function EyeTrackingLoadQuestionsPage(): React.ReactElement {
-  const { setState } = useStateStore.getState()
+  const { setState } = useStateStore()
   const navigate = useNavigate()
+
   // TODO: Remove once state is split up
   useEffect(() => {
     setState({
@@ -18,6 +20,43 @@ export function EyeTrackingLoadQuestionsPage(): React.ReactElement {
     })
   }, [setState])
 
+  function removeQuestionFile(_file: File) {
+    // Remove questions from state when file is removed
+    setState({ questions: undefined })
+  }
+
+  async function handleLoad() {
+    // Check if there are files to load (FileImport component manages this)
+    // We need to load the questions file first, then navigate
+    const { questions: currentQuestions } = useStateStore.getState()
+
+    if (
+      !currentQuestions ||
+      (Array.isArray(currentQuestions) && currentQuestions.length === 0)
+    ) {
+      const msg = 'No questions file to load. Please drop a questions file first.'
+      errorAlert(msg)
+      console.error(msg)
+      return
+    }
+
+    // Questions are already loaded (from the drop), just navigate
+    navigate(ROUTES.EYE_TRACKING_EXPERIMENT)
+  }
+
+  function handleDrop(files: File[], config: LoadFileConfig) {
+    if (files.length > 1) {
+      const msg = 'only a single file can be imported'
+      console.error(msg)
+      errorAlert(msg)
+      return
+    }
+
+    // Load the questions file immediately when dropped
+    // This will populate the questions in state, which allows the file item to be displayed
+    loadFiles(files, config)
+  }
+
   return (
     <FileImport
       mode="data-collection"
@@ -25,19 +64,15 @@ export function EyeTrackingLoadQuestionsPage(): React.ReactElement {
       expectedArtifact="questions"
       expectedExtensions={['csv']}
       uploadLabel="Drop a questions csv file"
-      onDrop={(files, config) => {
-        if (files.length > 1) {
-          const msg = 'only a single file can be imported' // check third argument
-          console.error(msg)
-          errorAlert(msg)
-          return
-        }
-
-        loadFiles(files, config)
-      }}
-      onLoad={() => {
-        navigate(ROUTES.EYE_TRACKING_EXPERIMENT)
-      }}
+      onDrop={handleDrop}
+      onLoad={handleLoad}
+      onRemove={removeQuestionFile}
+      renderItemContent={(file) => <QuestionFileItem file={file} />}
     />
   )
+}
+
+function QuestionFileItem({ file }: { file: File }) {
+  // Always show the file name - the file is in the FileImport component's local state
+  return <div className="column file-info">{file.name}</div>
 }

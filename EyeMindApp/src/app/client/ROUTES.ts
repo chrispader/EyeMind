dab790b932eb46ef90254882635693797c412c49
Christoph Pader <christoph.pader@outlook.com>
2025-12-27 15:04:41 +0000

    refactor: formatting

--- EyeMindApp/src/app/client/ROUTES.ts
+++ EyeMindApp/src/app/client/ROUTES.ts
@@ -1,11 +1,13 @@
 export const ROUTES = {
   HOME: '/',
+
   EYE_TRACKING: '/eye-tracking',
   EYE_TRACKING_NEW_SESSION: '/eye-tracking/new-session',
   EYE_TRACKING_NEW_LOAD_MODELS: '/eye-tracking/load-models',
   EYE_TRACKING_LOAD_SESSION: '/eye-tracking/load-session',
   EYE_TRACKING_LOAD_QUESTIONS: '/eye-tracking/load-questions',
   EYE_TRACKING_EXPERIMENT: '/eye-tracking/experiment',
+
   ANALYSIS: '/analysis',
   FIXATION_SETTINGS: '/fixation-settings',
   HEATMAP_SETTINGS: '/heatmap-settings',
--- EyeMindApp/src/app/client/components/HeatmapSettingsModal.tsx
+++ EyeMindApp/src/app/client/components/HeatmapSettingsModal.tsx
@@ -2,206 +2,204 @@ import React from 'react'
 
 export function HeatmapSettingsModal(): React.ReactElement {
   return (
-    <div id="heatmap-settings-modal" className="heatmap-settings-modal">
-      <div className="content">
-        <span className="close" id="close-heatmap-settings">
-          <img className="close-icon" id="close-icon" src="icons/close.svg" alt="Close" />
+    <div id='heatmap-settings-modal' className='heatmap-settings-modal'>
+      <div className='content'>
+        <span className='close' id='close-heatmap-settings'>
+          <img className='close-icon' id='close-icon' src='icons/close.svg' alt='Close' />
         </span>
 
         <h2>Heatmap Settings</h2>
 
         <div>
-          <div className="row">
-            <div className="column">
-              <span className="text">Participant (File) </span>
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Participant (File) </span>
             </div>
-            <div className="column">
+            <div className='column'>
               <select
-                className="form-select-multiple"
+                className='form-select-multiple'
                 multiple
-                id="participants-files-heatmap"></select>
+                id='participants-files-heatmap'></select>
             </div>
           </div>
 
-          <div className="row">
-            <div className="column">
-              <span className="text">Question ID: </span>
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Question ID: </span>
             </div>
-            <div className="column">
-              <select className="form-select" id="question">
-                <option value="">Select</option>
+            <div className='column'>
+              <select className='form-select' id='question'>
+                <option value=''>Select</option>
               </select>
             </div>
           </div>
 
-          <div className="row">
-            <div className="column">
-              <span className="text">Measure: </span>
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Measure: </span>
             </div>
-            <div className="column">
-              <select className="form-select" id="measure">
-                <option value="" data-measure-type="" data-aggregations="">
+            <div className='column'>
+              <select className='form-select' id='measure'>
+                <option value='' data-measure-type='' data-aggregations=''>
                   Select
                 </option>
                 <option
-                  value="visit_duration"
-                  data-measure-type="element_level"
-                  data-aggregations="sum-max-min-mean">
+                  value='visit_duration'
+                  data-measure-type='element_level'
+                  data-aggregations='sum-max-min-mean'>
                   Visit Duration (From Fixations)
                 </option>
                 <option
-                  value="visit_count"
-                  data-measure-type="element_level"
-                  data-aggregations="count">
+                  value='visit_count'
+                  data-measure-type='element_level'
+                  data-aggregations='count'>
                   Visit Count (From Fixations)
                 </option>
                 <option
-                  value="visit_duration"
-                  data-measure-type="gaze_level"
-                  data-aggregations="sum-max-min-mean">
+                  value='visit_duration'
+                  data-measure-type='gaze_level'
+                  data-aggregations='sum-max-min-mean'>
                   Visit Duration (From Gazes)
                 </option>
                 <option
-                  value="visit_count"
-                  data-measure-type="gaze_level"
-                  data-aggregations="count">
+                  value='visit_count'
+                  data-measure-type='gaze_level'
+                  data-aggregations='count'>
                   Visit Count (From Gazes)
                 </option>
                 <option
-                  value="Fixation Duration"
-                  data-measure-type="fixation_level"
-                  data-aggregations="sum-max-min-mean">
+                  value='Fixation Duration'
+                  data-measure-type='fixation_level'
+                  data-aggregations='sum-max-min-mean'>
                   Fixation Duration
                 </option>
                 <option
-                  value="Fixation Count"
-                  data-measure-type="fixation_level"
-                  data-aggregations="count">
+                  value='Fixation Count'
+                  data-measure-type='fixation_level'
+                  data-aggregations='count'>
                   Fixation Count
                 </option>
               </select>
             </div>
           </div>
 
-          <div className="row">
-            <div className="column">
-              <span className="text">Aggregation function: </span>
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Aggregation function: </span>
             </div>
-            <div className="column">
-              <select className="form-select" id="aggregation">
-                <option id="no-aggr" value="" data-aggregation-type="" className="">
+            <div className='column'>
+              <select className='form-select' id='aggregation'>
+                <option id='no-aggr' value='' data-aggregation-type='' className=''>
                   Select
                 </option>
                 <option
-                  id="sum-aggr"
-                  value="sum"
-                  data-aggregation-type="time"
-                  className="aggr">
+                  id='sum-aggr'
+                  value='sum'
+                  data-aggregation-type='time'
+                  className='aggr'>
                   Sum
                 </option>
                 <option
-                  id="max-aggr"
-                  value="max"
-                  data-aggregation-type="time"
-                  className="aggr">
+                  id='max-aggr'
+                  value='max'
+                  data-aggregation-type='time'
+                  className='aggr'>
                   Max
                 </option>
                 <option
-                  id="min-aggr"
-                  value="min"
-                  data-aggregation-type="time"
-                  className="aggr">
+                  id='min-aggr'
+                  value='min'
+                  data-aggregation-type='time'
+                  className='aggr'>
                   Min
                 </option>
                 <option
-                  id="mean-aggr"
-                  value="mean"
-                  data-aggregation-type="time"
-                  className="aggr">
+                  id='mean-aggr'
+                  value='mean'
+                  data-aggregation-type='time'
+                  className='aggr'>
                   Mean
                 </option>
                 <option
-                  id="count-aggr"
-                  value="count"
-                  data-aggregation-type="number"
-                  className="aggr">
+                  id='count-aggr'
+                  value='count'
+                  data-aggregation-type='number'
+                  className='aggr'>
                   Count
                 </option>
               </select>
             </div>
           </div>
 
-          <div className="row">
-            <div className="column">
-              <span className="text">Timestamp unit: </span>
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Timestamp unit: </span>
             </div>
-            <div className="column">
-              <select className="form-select" id="timestamp-unit">
-                <option value="s">Second</option>
-                <option value="ms" selected>
-                  Millisecond
-                </option>
-                <option value="us">Microsecond</option>
+            <div className='column'>
+              <select className='form-select' id='timestamp-unit' defaultValue='ms'>
+                <option value='s'>Second</option>
+                <option value='ms'>Millisecond</option>
+                <option value='us'>Microsecond</option>
               </select>
             </div>
           </div>
 
-          <div className="row">
-            <div className="column">
-              <span className="text">Additionally include pools and lanes:</span>
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Additionally include pools and lanes:</span>
             </div>
-            <div className="column">
-              <input className="form-check-box" type="checkbox" id="inc-pools-lanes" />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="column">
-              <span className="text">Additionally include groups (border only):</span>
-            </div>
-            <div className="column">
-              <input className="form-check-box" type="checkbox" id="inc-groups" />
+            <div className='column'>
+              <input className='form-check-box' type='checkbox' id='inc-pools-lanes' />
             </div>
           </div>
 
-          <div className="row">
-            <div className="column">
-              <span className="text">Additionally include expended sub-processes:</span>
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Additionally include groups (border only):</span>
             </div>
-            <div className="column">
+            <div className='column'>
+              <input className='form-check-box' type='checkbox' id='inc-groups' />
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Additionally include expended sub-processes:</span>
+            </div>
+            <div className='column'>
               <input
-                className="form-check-box"
-                type="checkbox"
-                id="inc-expended-sub-processes"
+                className='form-check-box'
+                type='checkbox'
+                id='inc-expended-sub-processes'
               />
             </div>
           </div>
 
-          <div className="row">
-            <div className="column">
-              <span className="text">Additionally include processes:</span>
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Additionally include processes:</span>
             </div>
-            <div className="column">
-              <input className="form-check-box" type="checkbox" id="inc-processes" />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="column">
-              <span className="text">Additionally include edges:</span>
-            </div>
-            <div className="column">
-              <input className="form-check-box" type="checkbox" id="inc-edges" />
+            <div className='column'>
+              <input className='form-check-box' type='checkbox' id='inc-processes' />
             </div>
           </div>
 
-          <div className="row">
+          <div className='row'>
+            <div className='column'>
+              <span className='text'>Additionally include edges:</span>
+            </div>
+            <div className='column'>
+              <input className='form-check-box' type='checkbox' id='inc-edges' />
+            </div>
+          </div>
+
+          <div className='row'>
             <div style={{ textAlign: 'center' }}>
               <input
-                type="submit"
-                className="submit-form-button"
-                id="submit-heatmap-form"
-                value="Show heatmap"
+                type='submit'
+                className='submit-form-button'
+                id='submit-heatmap-form'
+                value='Show heatmap'
               />
             </div>
           </div>
